969e8338b4a60ce0538393dbf86831869f4927be
PabloBlancoArg <blancopablo1991@gmail.com>
2020-11-16 14:50:41 -0300

    paste 1st project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :loans
+  resources :books
+  devise_for :users
+  resources :friends
+  #get 'home/index'
+  get 'home/about'
+  #root 'home#index'
+  root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
